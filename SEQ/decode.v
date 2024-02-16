5f45e05f1ff64dcc74dcc777d4b26bd35a9bf72c
gomu-abhi <sharma.abhishek@students.iiit.ac.in>
2024-02-16 18:27:53 +0530

    initial decode commit

---
+++ SEQ/decode.v
@@ -0,0 +1,25 @@
+module decode(valA, valB, rA, rB, icode, reg_file, clk)
+always @posedge clk
+begin
+    if(icode == 2 || icode ==4 || icode == 6 || icode == 10)
+    begin
+        srcA <= rA;
+    end
+    if(icode == 3 || icode == 4 || icode == 5 || icode == 6)
+    begin
+        scrB <= rB;
+    end
+    if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
+    begin
+        scrB <= rsp;
+    end
+    if(icode == 11 || icode == 9)
+    begin
+        srcA <= rsp;
+    end
+    valA <= reg_file[srcA];
+    valB <= reg_file[srcB];
+end
+
+
+endmodule

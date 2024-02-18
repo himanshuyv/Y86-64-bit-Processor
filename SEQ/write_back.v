5d9d76a542451c9fc52a70a469d97433d7ccfab4
gomu-abhi <sharma.abhishek@students.iiit.ac.in>
2024-02-19 01:19:33 +0530

    wr_back

--- SEQ/write_back.v
+++ SEQ/write_back.v
@@ -1,22 +1,25 @@
 module write_back(dstE, dstM, rA, rB, icode, clk)
-if(icode == 6 || icode == 2 || icode == 3)
-begin
-    dstE <= rB;
-end
-if(icode == 5 || icode == 11)
-begin
-    dstM <= rA;
-end
-if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
-begin
-    dstE <= rA;
-end
-if(icode == 0 || icode == 1 || icode == 4 || icode == 5 || icode == 6 || icode == 7)
-begin
-    dstE <= b'1111;
-end
-if(icode == 0 || icode == 1 || icode == 2 || icode == 3 || icode == 4 || icode == 6 || icode == 7 || icode == 8 || icode == 9 || icode == 10)
-begin
-    dstM <= b'1111;
-end
+
+    always @(*) begin
+        if(icode == 6 || icode == 2 || icode == 3)
+        begin
+            dstE <= rB;
+        end
+        if(icode == 5 || icode == 11)
+        begin
+            dstM <= rA;
+        end
+        if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
+        begin
+            dstE <= rA;
+        end
+        if(icode == 0 || icode == 1 || icode == 4 || icode == 5 || icode == 6 || icode == 7)
+        begin
+            dstE <= b'1111;
+        end
+        if(icode == 0 || icode == 1 || icode == 2 || icode == 3 || icode == 4 || icode == 6 || icode == 7 || icode == 8 || icode == 9 || icode == 10)
+        begin
+            dstM <= b'1111;
+        end
+    end
 endmodule

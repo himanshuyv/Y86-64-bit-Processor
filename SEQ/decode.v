6e3d0473faaa02988aec41169063c111c165e374
gomu-abhi <sharma.abhishek@students.iiit.ac.in>
2024-02-19 00:50:21 +0530

    decode

--- SEQ/decode.v
+++ SEQ/decode.v
@@ -1,25 +1,24 @@
-module decode(valA, valB, rA, rB, icode, reg_file, clk)
-always @posedge clk
-begin
-    if(icode == 2 || icode ==4 || icode == 6 || icode == 10)
+module decode(valA, valB, rA, rB, icode, clk)
+
+    always @(*)
     begin
-        srcA <= rA;
+        if(icode == 2 || icode ==4 || icode == 6 || icode == 10)
+        begin
+            srcA <= rA;
+        end
+        if(icode == 3 || icode == 4 || icode == 5 || icode == 6)
+        begin
+            scrB <= rB;
+        end
+        if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
+        begin
+            scrB <= rsp;
+        end
+        if(icode == 11 || icode == 9)
+        begin
+            srcA <= rsp;
+        end
     end
-    if(icode == 3 || icode == 4 || icode == 5 || icode == 6)
-    begin
-        scrB <= rB;
-    end
-    if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
-    begin
-        scrB <= rsp;
-    end
-    if(icode == 11 || icode == 9)
-    begin
-        srcA <= rsp;
-    end
-    valA <= reg_file[srcA];
-    valB <= reg_file[srcB];
-end
 
 
 endmodule

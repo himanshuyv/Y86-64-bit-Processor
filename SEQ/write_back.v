febccedec3ea0bb07470323ffa39cdf3929da75e
gomu-abhi <sharma.abhishek@students.iiit.ac.in>
2024-02-19 01:16:56 +0530

    write_back

--- SEQ/write_back.v
+++ SEQ/write_back.v
@@ -11,4 +11,12 @@ if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
 begin
     dstE <= rA;
 end
+if(icode == 0 || icode == 1 || icode == 4 || icode == 5 || icode == 6 || icode == 7)
+begin
+    dstE <= b'1111;
+end
+if(icode == 0 || icode == 1 || icode == 2 || icode == 3 || icode == 4 || icode == 6 || icode == 7 || icode == 8 || icode == 9 || icode == 10)
+begin
+    dstM <= b'1111;
+end
 endmodule

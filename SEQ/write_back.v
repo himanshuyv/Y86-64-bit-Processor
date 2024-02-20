a305fcd03af3b3d5d62cb69e7db9eb5aa7a6e392
gomu-abhi <sharma.abhishek@students.iiit.ac.in>
2024-02-20 19:54:48 +0530

    write_back cond updated

--- SEQ/write_back.v
+++ SEQ/write_back.v
@@ -10,19 +10,19 @@ module write_back(dstE, dstM, rA, rB, icode);
         begin
             dstE = rB;
         end
+        else if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
+        begin
+            dstE = 4;
+        end
+        else
+        begin
+            dstE = 15;
+        end
         if(icode == 5 || icode == 11)
         begin
             dstM = rA;
         end
-        if(icode == 10 || icode == 11 || icode == 8 || icode == 9)
-        begin
-            dstE = rA;
-        end
-        if(icode == 0 || icode == 1 || icode == 4 || icode == 5 || icode == 6 || icode == 7)
-        begin
-            dstE = 15;
-        end
-        if(icode == 0 || icode == 1 || icode == 2 || icode == 3 || icode == 4 || icode == 6 || icode == 7 || icode == 8 || icode == 9 || icode == 10)
+        else
         begin
             dstM = 15;
         end

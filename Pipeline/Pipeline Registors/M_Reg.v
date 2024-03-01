9cef8b00ef35e06483e62b1676c3d35625204501
Himanshu Yadav <hy815088@gmail.com>
2024-03-02 01:10:06 +0530

    modified M_reg

--- Pipeline/Pipeline Registors/M_Reg.v
+++ Pipeline/Pipeline Registors/M_Reg.v
@@ -1,18 +1,18 @@
 module M_Reg(M_stat,M_icode,M_Cnd,M_valE,M_valA,M_dstE,M_dstM,e_stat,e_icode,e_Cnd,e_valE,e_valA,e_dstE,e_dstM,clk);
-    output reg [2:0] e_stat;
-    output reg [3:0] e_icode;
-    output reg e_Cnd;
-    output reg [63:0] e_valE;
-    output reg [63:0] e_valA;
-    output reg [3:0] e_dstE;
-    output reg [3:0] e_dstM;
-    input [2:0] M_stat;
-    input [3:0] M_icode;
-    input M_Cnd;
-    input [63:0] M_valE;
-    input [63:0] M_valA;
-    input [3:0] M_dstE;
-    input [3:0] M_dstM;
+    output reg [2:0] M_stat;
+    output reg [3:0] M_icode;
+    output reg M_Cnd;
+    output reg [63:0] M_valE;
+    output reg [63:0] M_valA;
+    output reg [3:0] M_dstE;
+    output reg [3:0] M_dstM;
+    input [2:0] e_stat;
+    input [3:0] e_icode;
+    input e_Cnd;
+    input [63:0] e_valE;
+    input [63:0] e_valA;
+    input [3:0] e_dstE;
+    input [3:0] e_dstM;
     input clk;
     always @(*)
     begin
